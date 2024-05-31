22a76d6c11ba77ff9e7f1b6d9921cdd15f5785f2
Alessandro Spoto <alecodx@gmail.com>
2024-05-31 17:28:09 -0300

    update

---
+++ src/app/cadastrar-usuario.usecases.spec.js
@@ -0,0 +1,11 @@
+describe('Cadastrar usuario UseCase', () => {
+  test('Deve poder cadastrar um usuario',  () => {
+    const usuarioDTO = {
+      nome_completo: 'nome_valido',
+      cpf: 'cpf valido',
+      telefone: 'telefone valido',
+      endereco: 'endereço valido',
+      email: 'email valido'
+    }
+  })
+})
